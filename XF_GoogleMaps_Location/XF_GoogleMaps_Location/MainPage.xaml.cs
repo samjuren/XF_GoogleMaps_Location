ba4368831ae8f448fb4b78c04e8632c55d5159f0
Samuel Juren <sammjureen@outlook.com>
2023-07-24 23:21:27 -0300

    Add project files.

---
+++ XF_GoogleMaps_Location/XF_GoogleMaps_Location/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace XF_GoogleMaps_Location
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Pin pinPaulista = new Pin()
+            {
+                Label="Paulista",
+                Position = new Position(-23.4914908, -46.718843)
+            };
+
+            maps.Pins.Add(pinPaulista);
+            maps.MoveToRegion(MapSpan.FromCenterAndRadius(pinPaulista.Position, Distance.FromMeters(1000)));
+        }
+    }
+}
+    
